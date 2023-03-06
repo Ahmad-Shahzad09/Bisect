4483006a79a3e848efa175d5b359bb22707da0f1
Ahmad Shahzad <ahmad@Ahmad-Shahzad.local>
2023-03-06 11:05:02 +0500

    initial commit

---
+++ src/main/kotlin/com/example/git/GitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.git
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GitApplication
+
+fun main(args: Array<String>) {
+	runApplication<GitApplication>(*args)
+}
